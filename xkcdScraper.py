e57f4f3c7d141f189f6c42c2ed2259b656b07591
Stefan Hyltoft <stefanhyltoft@gmail.com>
2016-08-19 15:18:10 +0200

    Initial commit

---
+++ xkcdScraper.py
@@ -0,0 +1,76 @@
+import requests, bs4
+
+#Let's go to the frontpage and get the first image, and the first prev number!
+
+xkcdUrl = 'http://xkcd.com'
+
+res = requests.get(xkcdUrl)
+
+#Check for errors
+res.raise_for_status()
+
+xkcdSoup = bs4.BeautifulSoup(res.text)
+
+#Select the image element
+imgElem = xkcdSoup.select('#comic img')
+
+#Get img url
+imgUrl = imgElem[0].attrs['src']
+
+
+#Download the image
+imgRequest = requests.get('http:'+ imgUrl)
+
+if imgRequest.status_code == 200:
+    with open('latest.png', 'wb') as f:
+        for chunk in imgRequest:
+            f.write(chunk)
+
+f.close()
+
+print("Done scraping latest image")
+
+#Now lets get the previous comics!
+
+#Get the latest prev link
+prevLink = xkcdSoup.select('.comicNav li a[rel="prev"]')[0].attrs['href']
+
+#Get the next page and it's comic
+res = requests.get(xkcdUrl + prevLink)
+
+res.raise_for_status()
+
+xkcdSoup = bs4.BeautifulSoup(res.text)
+
+imgElem = xkcdSoup.select('#comic img')
+
+imgUrl = imgElem[0].attrs['src']
+comicNumber = prevLink.replace('/', "")
+
+imgRequest = requests.get('http:' + imgUrl)
+
+if imgRequest.status_code == 200:
+    with open(comicNumber + '.png', 'wb') as f:
+        for chunk in imgRequest:
+            f.write(chunk)
+f.close()
+
+print("Done scraping comic number: " + comicNumber)
+
+while (1 < comicNumber):
+    prevLink = xkcdSoup.select('.comicNav li a[rel="prev"]')[0].attrs['href']
+    res = requests.get(xkcdUrl + prevLink)
+    res.raise_for_status()
+    xkcdSoup = bs4.BeautifulSoup(res.text)
+    imgElem = xkcdSoup.select('#comic img')
+    imgUrl = imgElem[0].attrs['src']
+    comicNumber = prevLink.replace('/', "")
+    imgRequest = requests.get('http:' + imgUrl)
+    if imgRequest.status_code == 200:
+        with open(comicNumber, 'wb') as f:
+            for chunk in imgRequest:
+                f.write(chunk)
+    f.close()
+    print("Done scraping comic number: " + comicNumber)
+
+print("Done scraping all xkcd comics!")
